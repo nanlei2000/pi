8f42b27faf55f0be8be5f19e6526a78e6cf27343
nanlei <36439285+nanlei2000@users.noreply.github.com>
2022-06-30 22:16:09 +0800

    Remove new in for loop

--- pi.go
+++ pi.go
@@ -8,13 +8,16 @@ import (
 var (
 	n25    = big.NewInt(-25)
 	n57121 = big.NewInt(-57121)
+	p10    = big.NewInt(10)
+	zero   = big.NewInt(0)
 )
 
+// https://zhuanlan.zhihu.com/p/114320417, see part 3
 func Calc(n uint64) *big.Int {
 	t := n + 10
 
 	b := new(big.Int)
-	b.Exp(big.NewInt(10), big.NewInt(int64(t)), nil)
+	b.Exp(p10, big.NewInt(int64(t)), nil)
 
 	x1 := new(big.Int)
 	x1.Mul(b, big.NewInt(4))
@@ -27,11 +30,10 @@ func Calc(n uint64) *big.Int {
 	s.Add(x1, x2)
 
 	n *= 2
-
+	x := new(big.Int)
 	for i := uint64(3); i < n; i += 2 {
 		FloorDiv(x1, x1, n25)
 		FloorDiv(x2, x2, n57121)
-		x := new(big.Int)
 
 		x.Add(x1, x2)
 		FloorDiv(x, x, big.NewInt(int64(i)))
@@ -39,7 +41,7 @@ func Calc(n uint64) *big.Int {
 	}
 
 	s.Mul(s, big.NewInt(4))
-	FloorDiv(s, s, new(big.Int).Exp(big.NewInt(10), big.NewInt(10), nil))
+	FloorDiv(s, s, new(big.Int).Exp(p10, p10, nil))
 
 	return s
 }
@@ -47,7 +49,7 @@ func Calc(n uint64) *big.Int {
 func FloorDiv(z, x, y *big.Int) {
 	m := new(big.Int)
 	z.DivMod(x, y, m)
-	if z.Cmp(big.NewInt(0)) == -1 && m.Cmp(big.NewInt(0)) == +1 {
+	if z.Cmp(zero) == -1 && m.Cmp(zero) == +1 {
 		z.Add(z, big.NewInt(-1))
 	}
 }
